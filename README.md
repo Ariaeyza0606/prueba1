71d9621509b8706433e4b0b7c01455d67d76b3db
ariaeyza0606 <ariadna.eyzaguirre@gmail.com>
2016-08-25 18:11:04 -0500

    lista

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# prueba1
+# Prueba1
+Inicio
+n=número
+n>0 (no)-> n es negativo ->fin
+(si)
+n es positivo
+fin
+![alt](http://2.1m.yt/6an2RRK.jpg)
---
+++ app.js
@@ -0,0 +1,7 @@
+var num = parseInt(prompt ("Ingrese un numero"));
+if(num>0){
+    document.write ("positivo");
+}
+else{
+    document.write("negativo");
+}
